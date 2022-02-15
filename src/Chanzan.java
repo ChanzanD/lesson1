ec1ab10ddc3f089f3fe803704ca6a676f4f85190
ChanzanD <chanzan.dagby9374@gmail.com>
2022-02-15 22:33:32 +0700

    1.0

---
+++ src/Chanzan.java
@@ -0,0 +1,15 @@
+class Furnace {
+    int cobblestone = 8;
+    int capacity = 64;
+    int bakeMeat(int temperature) {
+        System.out.println(temperature);
+    return cobblestone;
+    }
+}
+public class Chanzan {
+    public static void main(String[] args) {
+        Furnace furnace = new Furnace();
+        furnace.bakeMeat(90);
+        furnace.bakeMeat(80);
+    }
+}
